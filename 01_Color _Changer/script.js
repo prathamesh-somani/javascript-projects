b6c8a2406d5a4da44546054c9eb06bf3b1cb2d93
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-01-16 17:52:22 +0000

    Project Complete

--- 01_Color _Changer/script.js
+++ 01_Color _Changer/script.js
@@ -8,20 +8,41 @@ buttons.forEach ((button)=>{
         console.log(e);
         console.log(e.target);
 
-        if (e.target.id === "grey"){
-            body.style.backgroundColor= e.target.id;
-        }
-        if (e.target.id === "blue"){
-            body.style.backgroundColor= e.target.id;
-        }
-        if (e.target.id === "white"){
-            body.style.backgroundColor= e.target.id;
-        }
-        if (e.target.id === "yellow"){
-            body.style.backgroundColor= e.target.id;
-        }
-        if (e.target.id === "purple"){
-            body.style.backgroundColor= e.target.id;
+        // if (e.target.id === "grey"){
+        //     body.style.backgroundColor= e.target.id;
+        // }
+        // if (e.target.id === "blue"){
+        //     body.style.backgroundColor= e.target.id;
+        // }
+        // if (e.target.id === "white"){
+        //     body.style.backgroundColor= e.target.id;
+        // }
+        // if (e.target.id === "yellow"){
+        //     body.style.backgroundColor= e.target.id;
+        // }
+        // if (e.target.id === "purple"){
+        //     body.style.backgroundColor= e.target.id;
+        // }
+
+        switch (e.target.id) {
+            case "grey":
+                body.style.backgroundColor= e.target.id;
+                break;
+            case "yellow":
+                body.style.backgroundColor= e.target.id;
+                break;
+            case "blue":
+                body.style.backgroundColor= e.target.id;
+                break;
+            case "white":
+                body.style.backgroundColor= e.target.id;
+                break;
+            case "purple":
+                body.style.backgroundColor= e.target.id;
+                break;
+        
+            default:
+                break;
         }
 
 
